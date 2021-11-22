bddc43533481efb8100c4e4392d11edb5bbfc59c
iquidus <luke@iquidus.io>
2021-11-22 17:23:53 -0500

    bump default GasCeil to 12million

--- eth/ethconfig/config.go
+++ eth/ethconfig/config.go
@@ -85,7 +85,7 @@ var Defaults = Config{
 	TrieTimeout:             60 * time.Minute,
 	SnapshotCache:           102,
 	Miner: miner.Config{
-		GasCeil:  8000000,
+		GasCeil:  12000000,
 		GasPrice: big.NewInt(80 * params.GWei),
 		Recommit: 3 * time.Second,
 	},
